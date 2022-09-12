7f98cd1c93f740fe2c6f88d7edc1e8ebd988b818
Giuliano Conti <giuliconti1@gmail.com>
2022-09-12 11:57:31 -0300

    contact changes

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "./Contact.css";
 
 export const Contact = () => {
-  return <div>Contact joaco troll</div>;
+  return (
+    <div className="contact-screen">
+      <h1 className="contact-title">Contact</h1>
+      <p className="contact-subtitle">Our users are very satisfied about our excellent support.</p>
+      <form className="contact-form">
+        <input className="contact-form-input" type="text" placeholder="Name" />
+        <input className="contact-form-input" type="email" placeholder="Email" />
+        <textarea className="contact-form-textarea" placeholder="Message" />
+      </form>
+    </div>
+  );
 };
