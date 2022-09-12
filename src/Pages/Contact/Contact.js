8d0bdc64be8e29bb11cbcdf4ddba1b4b155fca05
JoaGal <joaquingaldeano7@gmail.com>
2022-09-12 19:12:56 -0300

    Commit

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Contact.css";
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+  const name = useRef();
+  const email = useRef();
+  const message = useRef();
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    if (
+      name.current.value.length >= 5 &&
+      email.current.value.length >= 10 &&
+      message.current.value.length >= 10
+    ) {
+      emailjs.sendForm(
+          "service_c20g5o8",
+          "template_fj3uvs6",
+          form.current,
+          "Txxmygclbsvhn59mu"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            alert("Message Send");
+          },
+          (error) => {
+            console.log(error.text);
+            alert("Message Not Send");
+          }
+        );
+      e.preventDefault();
+    } else {
+      e.preventDefault();
+      alert("Message Not Send");
+    }
+  };
+
   return (
     <div className="contact-screen">
       <h1 className="contact-title">Contact</h1>
-      <p className="contact-subtitle">Our users are very satisfied about our excellent support.</p>
-      <form className="contact-form">
-        <input className="contact-form-input" type="text" placeholder="Name" />
-        <input className="contact-form-input" type="email" placeholder="Email" />
-        <textarea className="contact-form-textarea" placeholder="Message" />
+      <p className="contact-subtitle">
+        Our users are very satisfied about our excellent support.
+      </p>
+      <form className="contact-form" onSubmit={handleSubmit} ref={form}>
+        <input className="contact-form-input" type="text" placeholder="Name" name="to_name" ref={name}/>
+        <input
+          className="contact-form-input"
+          type="email"
+          placeholder="Email"
+          name="from_name"
+          ref={email}
+        />
+        <textarea className="contact-form-textarea" placeholder="Message" name="message" ref={message}/>
+        <button type="submit" className="contact-form-button">Send</button>
       </form>
     </div>
   );
--- src/Pages/Templates/Templates.js
+++ src/Pages/Templates/Templates.js
@@ -1,10 +1,43 @@
 import React from "react";
 import "./Templates.css";
+import {Template1} from "../../Components/Templates/Template1/Template1"
+import {Template2} from "../../Components/Templates/Template2/Template2"
+import {Template3} from "../../Components/Templates/Template3/Template3"
 
 export const Templates = () => {
+  
+  const allTemplates = [
+    {
+      id: 1,
+      name: "Harvard",
+      templates: <Template1 />
+    },
+    {
+      id: 2,
+      name: "Edingburgh",
+      templates: <Template2 />
+    },
+    {
+      id: 3,
+      name: "Standford",
+      templates: <Template3 />
+    },
+  ];
+
+
   return (
     <div className="templates-screen">
-      <h1 className="templates-title">Templates</h1>
+      <h1 className="templates-title">Examples of professional CV</h1>
+      <p className="templates-subtitle">Increase your chances of finding a job and build your resume with one of our professionally designed resume templates.</p>
+      <button className="templates-button">Create CV</button>
+      <ul className="templates-ul">
+      {allTemplates.map((item) => (
+          <li className="templates-li" key={item.id}>
+              <h2 className="templates-li-h2">{item.name}</h2>
+              {item.templates}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
