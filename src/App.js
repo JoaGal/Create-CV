426d150d4af246ca1293ad1adb1431cfe0372eb8
Giuliano Conti <giuliconti1@gmail.com>
2022-09-12 11:41:14 -0300

    format document

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Home} from './Pages/Home/Home';
-import {Template} from './Pages/Template/Template';
-import {Contact} from './Pages/Contact/Contact';
-import {Navbar} from './Components/Navbar/Navbar';
-
+import { Home } from "./Pages/Home/Home";
+import { Template } from "./Pages/Template/Template";
+import { Contact } from "./Pages/Contact/Contact";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 export const App = () => {
   return (
     <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/template" element={<Template/>} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-  )
-}
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/template" element={<Template />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
