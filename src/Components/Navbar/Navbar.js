7a7e2726c4da5013380affc21a9f5ecd547cbac0
JoaGal <joaquingaldeano7@gmail.com>
2022-09-12 11:58:12 -0300

    Last

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { MdMenu } from 'react-icons/md';
+import './Navbar.css'
 
 export const Navbar = () => {
+ 
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div>Navbaraaaaaaaaa</div>
-  )
+    <div className='nav'>
+      <div
+        className="mobile-menu-icon"
+        onClick={() => setShowNav(!showNav)}
+        role="button"
+        onKeyDown={() => setShowNav(!showNav)}
+      >
+        <MdMenu />
+      </div>
+      <ul className={!showNav ? 'navItems hide-item' : 'navItems'}>
+        <li className='nav-li'>
+          <NavLink
+            to="/"
+            onClick={() => setShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => setShowNav(!showNav)}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className='nav-li'>
+          <NavLink
+            to="/template"
+            onClick={() => setShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => setShowNav(!showNav)}
+          >
+            Template
+          </NavLink>
+        </li>
+        <li className='nav-li'>
+          <NavLink
+            to="/contact"
+            onClick={() => setShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => setShowNav(!showNav)}
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
