c11dfa1a6542b75f7dacc9e25d9605839dde3d32
JoaGal <joaquingaldeano7@gmail.com>
2022-09-12 11:25:40 -0300

    Last Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home} from './Pages/Home/Home';
+import {Template} from './Pages/Template/Template';
+import {Contact} from './Pages/Contact/Contact';
+import {Navbar} from './Components/Navbar/Navbar';
+
 
 export const App = () => {
   return (
-    <div>hola</div>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/template" element={<Template/>} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
