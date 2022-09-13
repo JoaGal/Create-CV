4125bcc81049e7ebb2274df84f534ea76eb71619
JoaGal <joaquingaldeano7@gmail.com>
2022-09-13 19:19:51 -0300

    Commit

--- src/Components/PersonalInf/PersonalInf.js
+++ src/Components/PersonalInf/PersonalInf.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PersonalInf.css";
 import { AiFillCamera } from "react-icons/ai";
 import { MdAddCircleOutline } from "react-icons/md";
-
+import { AiOutlineMinusCircle } from "react-icons/ai";
 
 export const PersonalInf = () => {
+  const [moreInfo, setMoreInfo] = useState(false);
+
   return (
     <div className="personalInf-box">
       <h3 className="personalInf-title">Personal information</h3>
@@ -41,16 +43,60 @@ export const PersonalInf = () => {
       </div>
       <div className="personalInf-box3">
         <div className="personalInf-email">
-          <p className="personalInf-p">Postal Code</p>
+          <p className="personalInf-p">Country</p>
           <input type="text" className="personalInf-input" />
         </div>
         <div className="personalInf-number-phone">
-          <p className="personalInf-p">Country/City</p>
+          <p className="personalInf-p">Village/City</p>
           <input type="text" className="personalInf-input" />
         </div>
       </div>
-        <button className="personalInf-button-more"><MdAddCircleOutline className="personalInf-ico-button"/> Additional Information</button>
+
+      {moreInfo ? (
+        <>
+          <div className="personalInf-box3">
+            <div className="personalInf-email">
+              <p className="personalInf-p">Date of Birth</p>
+              <input type="date" className="personalInf-input" />
+            </div>
+            <div className="personalInf-number-phone">
+              <p className="personalInf-p">Marital status</p>
+              <input type="text" className="personalInf-input" />
+            </div>
+          </div>
+          <div className="personalInf-box3">
+            <div className="personalInf-email">
+              <p className="personalInf-p">Postal Code</p>
+              <input type="text" className="personalInf-input" />
+            </div>
+            <div className="personalInf-number-phone">
+              <p className="personalInf-p">Gender</p>
+              <input type="text" className="personalInf-input" />
+            </div>
+          </div>
+          <div className="personalInf-box3">
+            <div className="personalInf-email">
+              <p className="personalInf-p">LinkedIN</p>
+              <input type="text" className="personalInf-input" />
+            </div>
+            <div className="personalInf-number-phone">
+              <p className="personalInf-p">Briefcase</p>
+              <input type="text" className="personalInf-input" />
+            </div>
+          </div>
+        </>
+      ) : null}
+      <button
+        className="personalInf-button-more"
+        onClick={() => setMoreInfo(!moreInfo)}
+      >
+        {moreInfo ? (
+          <AiOutlineMinusCircle className="personalInf-ico-button" />
+        ) : (
+          <MdAddCircleOutline className="personalInf-ico-button" />
+        )}
+        Additional Information
+      </button>
     </div>
-    
   );
 };
