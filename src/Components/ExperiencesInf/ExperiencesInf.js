60609e9632ffad52232419573bf39cc442351449
JoaGal <joaquingaldeano7@gmail.com>
2022-09-15 11:11:08 -0300

    Commit

--- src/Components/ExperiencesInf/ExperiencesInf.js
+++ src/Components/ExperiencesInf/ExperiencesInf.js
@@ -10,6 +10,7 @@ const title = ["Work experience", "Profile", "Skill", "Languages", "Studies and 
 
 export const ExperiencesInf = () => {
   const [editSectionName, setEditSectionName] = useState(false);
+  const [reference, setReference] = useState(0)
 
   const closeEditSectionName = (e) => {
     console.log(e);
@@ -18,12 +19,32 @@ export const ExperiencesInf = () => {
     }
   };
 
+  const changeSectionName = (e)=>{
+    if (e.target.id.includes("0")) {
+      setReference(0)
+    }
+    else if (e.target.id.includes("1")) {
+      setReference(1)
+    }
+    else if (e.target.id.includes("2")) {
+      setReference(2)
+    }
+    else if (e.target.id.includes("3")) {
+      setReference(3)
+    }
+    else if (e.target.id.includes("4")) {
+      setReference(4)
+    }
+    setEditSectionName(true);
+  }
+
+
   return (
     <>
       {editSectionName && (
         <div className="experiencesInf-container" onClick={closeEditSectionName}>
           <div className="experiencesInf-box-open changeSectionName">
-            <input type="text" className="experiencesInf-h3 changeSectionName" value="hola" />
+            <input type="text" className="experiencesInf-h3 changeSectionName" placeholder={title[reference]} />
           </div>
           <button className="experiencesInf-button-fixed" onClick={closeEditSectionName}>
             X
@@ -35,8 +56,8 @@ export const ExperiencesInf = () => {
           <BsFillBriefcaseFill className="experiencesInf-ico" />
           {title[0]}
         </h3>
-        <button onClick={() => setEditSectionName(!editSectionName)}>
-          <FaWrench className="experiencesInf-ico-config" />
+        <button onClick={changeSectionName} id="0" className="experiencesInf-button-change">
+          <FaWrench className="experiencesInf-ico-config" id="0" onClick={()=> setReference(0)}/>
         </button>
       </div>
       <div className="experiencesInf-box">
@@ -44,28 +65,36 @@ export const ExperiencesInf = () => {
           <BsPersonSquare className="experiencesInf-ico" />
           {title[1]}
         </h3>
-        <FaWrench className="experiencesInf-ico-config" />
+        <button onClick={changeSectionName} id="1" className="experiencesInf-button-change">
+          <FaWrench className="experiencesInf-ico-config " id="1" onClick={()=> setReference(1)}/>
+        </button>
       </div>
       <div className="experiencesInf-box">
         <h3 className="experiencesInf-h3">
           <BsFillMouse2Fill className="experiencesInf-ico" />
           {title[2]}
         </h3>
-        <FaWrench className="experiencesInf-ico-config" />
+        <button onClick={changeSectionName} id="2" className="experiencesInf-button-change">
+          <FaWrench className="experiencesInf-ico-config " id="2" onClick={()=> setReference(2)}/>
+        </button>
       </div>
       <div className="experiencesInf-box">
         <h3 className="experiencesInf-h3">
           <BiWorld className="experiencesInf-ico" />
           {title[3]}
         </h3>
-        <FaWrench className="experiencesInf-ico-config" />
+        <button onClick={changeSectionName} id="3" className="experiencesInf-button-change">
+          <FaWrench className="experiencesInf-ico-config 3" id="3" onClick={()=> setReference(3)}/>
+        </button>
       </div>
       <div className="experiencesInf-box">
         <h3 className="experiencesInf-h3">
           <GiGraduateCap className="experiencesInf-ico" />
           {title[4]}
         </h3>
-        <FaWrench className="experiencesInf-ico-config" />
+        <button onClick={changeSectionName}  id="4" className="experiencesInf-button-change">
+          <FaWrench className="experiencesInf-ico-config" id="4" onClick={()=> setReference(4)}/>
+        </button>
       </div>
     </>
   );
