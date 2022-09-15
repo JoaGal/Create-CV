015634ac5222e99b5ef3d9c894d0509b9d555ed9
Giuliano Conti <giuliconti1@gmail.com>
2022-09-15 16:01:15 -0300

    templates changes

--- src/Components/Templates/Template1/Template1.js
+++ src/Components/Templates/Template1/Template1.js
@@ -1,7 +1,113 @@
-import React from 'react'
+import React from "react";
+import "./Template1.css";
 
 export const Template1 = () => {
   return (
-    <div>Template1</div>
-  )
-}
+    <div id="page-wrap">
+      <img className="template-1-img" src="https://avatars.githubusercontent.com/u/88043910?v=4" alt="Foto de perfil" id="pic" />
+
+      <div id="contact-info" className="vcard">
+        <h1 className="h1">Giuliano Conti</h1>
+
+        <p className="p">
+          CUIL: 20-43343429-4
+          <br />
+          Cel: +5493624223320
+          <br />
+          Domicilio: Pasaje Mármol 959, Resistencia, Chaco
+          <br />
+          Email:
+          <a className="email a" href="mailto:giuliconti1@gmail.com">
+            giuliconti1@gmail.com
+          </a>
+          <br />
+          Github:
+          <a className="linkedin a" href="https://github.com/GiuliannT">
+            https://github.com/GiuliannT
+          </a>
+          <br />
+          Linkedin:
+          <a className="linkedin a" href="https://www.linkedin.com/in/giulianoconti/">
+            https://www.linkedin.com/in/giulianoconti
+          </a>
+        </p>
+      </div>
+
+      <div id="objective">
+        <p>
+          Soy una persona autodidacta. Me gustan los desafíos. Tengo muchas ganas de unirme a un grupo de trabajo para
+          aportar, seguir aprendiendo y crecer con la programación.
+        </p>
+      </div>
+
+      <div className="clear"></div>
+
+      <dl>
+        <dd className="clear dd"></dd>
+
+        <dt className="dt">Educación</dt>
+        <dd className="dd">
+          <p className="p">
+            <strong>Primaria:</strong> Instituto Educativo Privado N° 2 (Arbo y Blanco 470, Resistencia, Chaco)
+            <br />
+            <strong>Secundaria:</strong> Título de Bachiller - EES N° 76 Colegio Nacional Jose Maria Paz (Av. 9 de Julio
+            640, Resistencia, Chaco)
+            <br />
+            <strong>Universidad:</strong> Primer año aprobado en Ingeniería civil - Facultad de Ingeniería - UNNE (Av.
+            Las Heras 727, Resistencia, Chaco)
+          </p>
+        </dd>
+
+        <dd className="clear dd"></dd>
+
+        <dt className="dt">Habilidades</dt>
+        <dd className="dd">
+          <h2 className="h2">Habilidades de programación</h2>
+          <p className="p">React, HTML5, CSS3, JavaScript y Git</p>
+
+          <h2 className="h2">Habilidades extras</h2>
+          <p className="p">
+            SketchUp, Software de productividad de Microsoft Office (Word, Excel, etc), Creación de mapas para juegos
+            FPS de Valve (Valve Hammer Editor), Linux (Ubuntu), Windows, Bueno googleando y
+            encontrando soluciones a diferentes problemas
+          </p>
+        </dd>
+
+        <dd className="clear dd"></dd>
+
+        <dt className="dt">Certificados</dt>
+        <dd className="dd">
+          <p className="p">
+            Curso de Desarrollo Web - Informatorio (Subsecretaría de Empleo del Chaco - SEC)
+            <br />
+            Curso de Introducción a la programación - Informatorio (SEC)
+            <br />
+            Curso Introducción al lenguaje de Programación - EducaciónIT (Alumni Education)
+            <br />
+            JavaScript for Beginners - The Complete introduction to JS - Udemy
+          </p>
+        </dd>
+
+        <dd className="clear dd"></dd>
+
+        <dt className="dt">Pasatiempos</dt>
+        <dd className="dd">
+          <p className="p">
+            Diseñar planos 3D en SketchUp
+            <br />
+            Juntarme con amigos
+            <br />
+            Jugar videojuegos
+            <br />
+            Andar en bicicleta
+            <br />
+          </p>
+        </dd>
+
+        <dd className="clear dd"></dd>
+      </dl>
+
+      <div className="clear"></div>
+    </div>
+  );
+};
--- src/Components/Templates/TemplatesInf.js
+++ src/Components/Templates/TemplatesInf.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import "./TemplatesInf.css";
 
 export const TemplatesInf = () => {
+  const allInformation = {
+    title: "Templates",
+    description: "This is the description of the Templates section",
+    icon: "https://img.icons8.com/ios/50/000000/stack-of-photos.png",
+  };
+
   return (
-    <div>TemplatesInf</div>
-  )
-}
+    <div className="templatesInf-container">
+      <div className="templatesInf-grid">
+        <div className="templatesInf-grid-item">
+          <h1 className="templatesInf-grid-item-title">Template 1</h1>
+          <iframe
+            className="templatesInf-grid-item-iframe"
+            src="/template1"
+            title="Template 1"
+            height="300"
+            width="280"
+          ></iframe>
+        </div>
+        <div className="templatesInf-grid-item">
+          <h1 className="templatesInf-grid-item-title">Template 2</h1>
+          <iframe
+            className="templatesInf-grid-item-iframe"
+            src="/template2"
+            title="Template 2"
+            height="300"
+            width="280"
+          ></iframe>
+        </div>
+        <div className="templatesInf-grid-item">
+          <h1 className="templatesInf-grid-item-title">Template 3</h1>
+          <iframe
+            className="templatesInf-grid-item-iframe"
+            src="/template3"
+            title="Template 3"
+            height="300"
+            width="280"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
