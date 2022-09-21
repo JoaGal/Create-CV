65bfe78ad5b812eff4c680dc91075e0324f01122
JoaGal <joaquingaldeano7@gmail.com>
2022-09-20 21:25:48 -0300

    Commit

--- src/Components/Templates/Template2/Template2.js
+++ src/Components/Templates/Template2/Template2.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import "./Template2.css";
 
-export const Template2 = () => {
+export const Template2 = ({ styleScale = "1" }) => {
+  const allInformation = JSON.parse(localStorage.getItem("allInformation"));
+
+  useEffect(() => {
+    if (window.location.href.includes("template2")) {
+      document.title = "Resume";
+      setTimeout(() => {
+        window.print();
+        window.close();
+      }, 1000);
+    }
+  }, []);
+
   return (
-    <div id="template2">Template2</div>
-  )
-}
+    <div
+      id="template2"
+      style={{
+        transform: `scale(${styleScale})`,
+        transformOrigin: `${styleScale === "0.24" ? "0 0" : "center"}`,
+      }}
+    >
+      <h1 className="template-1-h1">
+        {allInformation?.personal?.name} {allInformation?.personal?.lastname}
+      </h1>
+    </div>
+  );
+};
+
+Template2.propTypes = {
+  styleScale: PropTypes.string,
+  allInformation: PropTypes.object,
+};
