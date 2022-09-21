f1e36d85225247fcc70e561639f5e987ad365d10
Giuliano Conti <giuliconti1@gmail.com>
2022-09-21 12:24:00 -0300

    template 3 added

--- src/Components/Templates/Template3/Template3.js
+++ src/Components/Templates/Template3/Template3.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import "./Template3.css";
 
-export const Template3 = () => {
+export const Template3 = ({ styleScale = "1" }) => {
+  const allInformation = JSON.parse(localStorage.getItem("allInformation"));
+  useEffect(() => {
+    if (window.location.href.includes("template3")) {
+      document.title = "Resume";
+      setTimeout(() => {
+        window.print();
+        window.close();
+      }, 1000);
+    }
+  }, []);
+
   return (
-    <div id="template3">Template3</div>
-  )
-}
+    <div
+      id="template3"
+      style={{ transform: `scale(${styleScale})`, transformOrigin: `${styleScale === "0.24" ? "0 0" : "center"}` }}
+    >
+      <img
+        className="template-3-img"
+        src="https://avatars.githubusercontent.com/u/88043910?v=4"
+        alt="Foto de perfil"
+        id="pic"
+      />
+      <p>
+        {allInformation?.personal?.name} {allInformation?.personal.lastname}
+        {allInformation?.personal?.email} {allInformation?.personal?.phone}
+        {allInformation?.personal?.address} {allInformation?.personal?.country}
+        {allInformation?.personal?.city} {allInformation?.personal?.date}
+        {allInformation?.personal?.maritalStatus} {allInformation?.personal?.postalCode}
+        {allInformation?.personal?.gender} {allInformation?.personal?.linkedin}
+        {allInformation?.personal?.briefcase}
+      </p>
+
+      <p>
+        {allInformation?.experiences?.workExperience?.workstation}
+        {allInformation?.experiences?.workExperience?.city}
+        {allInformation?.experiences?.workExperience?.employer}
+        {allInformation?.experiences?.workExperience?.startWork}
+        {allInformation?.experiences?.workExperience?.finishWork}
+        {allInformation?.experiences?.workExperience?.descriptionWork}
+      </p>
+
+      <p>{allInformation?.experiences?.profile?.descriptionProfile}</p>
+
+      <p>
+        {allInformation?.experiences?.skill?.skill}
+        {allInformation?.experiences?.skill?.skillLevel}
+      </p>
+
+      <p>
+        {allInformation?.experiences?.lenguages?.lenguage}
+        {allInformation?.experiences?.lenguages?.lenguageLevel}
+      </p>
+
+      <p>
+        {allInformation?.experiences?.studies?.study}
+        {allInformation?.experiences?.studies?.startPeriod}
+        {allInformation?.experiences?.studies?.processPeriod}
+        {allInformation?.experiences?.studies?.descriptionStudy}
+      </p>
+    </div>
+  );
+};
+
+Template3.propTypes = {
+  styleScale: PropTypes.string,
+  allInformation: PropTypes.object,
+};
